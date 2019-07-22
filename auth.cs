eae89f7a1fa7836d97877655baaf27c5c5c25ecf
CardLife-Zhang <graham@murphys.be>
2019-07-22 11:27:54 +0100

    Add project files.

---
+++ auth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLifeAltLaunch
+{
+    class auth
+    {
+        public string PublicId { get; set; }
+        public string EmailAddress { get; set; }
+        public string DisplayName { get; set; }
+        public string Purchases { get; set; }
+        public string Flags { get; set; }
+        public string Confirmed { get; set; }
+        public string Token { get; set; }
+        public string SteamID { get; set; }
+        public string ID { get; set; }
+    }
+}
